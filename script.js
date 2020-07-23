82a4c2ee770df28661a2e0b93d9d43cfcc27421c
Pavel <wol1414@gmail.com>
2020-07-23 12:50:08 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+	$(".link-modal").fancybox({
+		arrows: false,
+		infobar: false,
+		touch: false,
+		type: 'inline',
+		autoFocus: false,
+		i18n: {
+			en: {
+				CLOSE: "Закрыть",
+				NEXT: "Вперед",
+				PREV: "Назад",
+				// PLAY_START: "Start slideshow",
+				// PLAY_STOP: "Pause slideshow",
+				// FULL_SCREEN: "Full screen",
+				// THUMBS: "Thumbnails",
+				// DOWNLOAD: "Download",
+				// SHARE: "Share",
+				// ZOOM: "Zoom"
+			},
+		},
+	});
